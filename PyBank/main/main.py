4b6fc19ca7ac0930ef27c45b36ee930ff1672796
Sarah Parzyck <sarahparzyck@gmail.com>
2020-05-26 18:04:58 -0500

    success first half

--- PyBank/main/main.py
+++ PyBank/main/main.py
@@ -10,8 +10,8 @@ def print_months(months_data):
 def print_total(total_data):
     print(f"Total: ${total_data}")
     
-#def print_average(average_change):
-#    print(f"Average Change: ${average_change}")
+def print_average(average_change):
+    print(f"Average Change: ${average_change}")
     
 def print_max(greatest_increase, date_increase):
     print(f"Greatest Increase in Profits: {date_increase} ${greatest_increase}")
@@ -32,6 +32,7 @@ with open(budget_csv, 'r') as csvfile:
     maxDateIncrease = ""
     minDateDecrease = ""
     averageChange = 0
+    firstRow = True
     for row in csvreader:
         count_of_months += 1
         net_total += int(row[1])
@@ -41,12 +42,15 @@ with open(budget_csv, 'r') as csvfile:
         if maxDecrease < previousRow - int(row[1]):
             maxDecrease = (previousRow - int(row[1]))
             minDateDecrease = row[0]
-#        averageChange += (previousRow - int(row[1]))
+        if firstRow:
+            firstRow = False
+        else:
+            averageChange += (int(row[1]) - previousRow)
+        
         previousRow = int(row[1])
-    
     print_months(count_of_months)
     print_total(net_total)
-#    print_average (averageChange / (count_of_months - 1))
+    print_average (averageChange / (count_of_months - 1))
     print_max(maxIncrease, maxDateIncrease)
     print_min(maxDecrease, minDateDecrease)
     
