c02ee318254fa2ef490c5a1a293f257090f5dbef
Sarah Parzyck <sarahparzyck@gmail.com>
2020-05-27 15:30:37 -0500

    progress

---
+++ PyBank/main.py
@@ -0,0 +1,60 @@
+import os
+import csv
+
+print("Financial Analysis")
+print("-----------------------")
+
+def print_months(months_data):
+    print(f"Total Months: {months_data}")
+    
+def print_total(total_data):
+    print(f"Total: ${total_data}")
+    
+def print_average(average_change):
+    print(f"Average Change: ${average_change}")
+    
+def print_max(greatest_increase, date_increase):
+    print(f"Greatest Increase in Profits: {date_increase} ${greatest_increase}")
+
+def print_min(greatest_decrease, date_decrease):
+    print(f"Greatest Decrease in Profits: {date_decrease} $-{greatest_decrease}")
+
+budget_csv = os.path.join('budget_data.csv')
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    
+    count_of_months = 0
+    net_total = 0
+    maxIncrease = 0
+    maxDecrease = 0
+    previousRow = 0
+    maxDateIncrease = ""
+    minDateDecrease = ""
+    averageChange = 0
+    firstRow = True
+    for row in csvreader:
+        count_of_months += 1
+        net_total += int(row[1])
+        if maxIncrease < int(row[1]) - previousRow:
+            maxIncrease = (int(row[1]) - previousRow)
+            maxDateIncrease = row[0]
+        if maxDecrease < previousRow - int(row[1]):
+            maxDecrease = (previousRow - int(row[1]))
+            minDateDecrease = row[0]
+        if firstRow:
+            firstRow = False
+        else:
+            averageChange += (int(row[1]) - previousRow)
+        
+        previousRow = int(row[1])
+    print_months(count_of_months)
+    print_total(net_total)
+    print_average (averageChange / (count_of_months - 1))
+    print_max(maxIncrease, maxDateIncrease)
+    print_min(maxDecrease, minDateDecrease)
+    
+
+        
+
+        
---
+++ PyPoll/mainpoll.py
@@ -0,0 +1,48 @@
+import os
+import csv
+
+print("Election Results")
+print("-----------------------")
+
+def print_votes(votes_data):
+    print(f"Total Votes: {votes_data}")
+
+def count_votes(list_of_all_votes, cand_name):
+    count = 0
+    for vote in list_of_all_votes:
+        if(vote == cand_name):
+            count += 1
+    return count
+
+election_csv = os.path.join('election_data.csv')
+with open(election_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    
+    count_of_votes = 0
+ 
+    cand_list = []
+    list_of_all_votes =[]
+    
+    for row in csvreader:
+        count_of_votes += 1
+        list_of_all_votes.append(row[2])
+        
+        cand_name = row[2]
+
+        if cand_name not in cand_list:
+            cand_list.append(cand_name)
+        
+        
+    
+    vote_dict = {}
+    for cand_name in cand_list:
+        votes_per_cand = count_votes(list_of_all_votes, cand_name)
+        vote_dict[cand_name] = votes_per_cand
+
+    print_votes(count_of_votes)
+    print("-----------------------")
+    print(str(vote_dict))
+    print("-----------------------")
+    
+
